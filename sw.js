2f18fc1b381734d846dbc66ed3d82441df6e7e18
Daniel Eskelsen <eskelsen@yahoo.com>
2026-05-01 23:19:32 -0300

    Adds manifest + update icon

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'calorias-passos-v1';
+const CACHE_NAME = 'gradus-v1';
 
 const ASSETS = [
   './',
-  './index.html'
+  './index.html',
+  './icon.png',
+  './manifest.webmanifest'
 ];
 
 self.addEventListener('install', event => {
@@ -11,6 +13,20 @@ self.addEventListener('install', event => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
